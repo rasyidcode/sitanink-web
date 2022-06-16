27746d159e3db58b2fad7cccacd8c1cdef621027
rasyidcode <rasyidcode7@gmail.com>
2022-06-16 13:40:19 +0700

    add excel export data wilayah

--- sitanink-webfiles/modules/admin/wilayah/controllers/WilayahController.php
+++ sitanink-webfiles/modules/admin/wilayah/controllers/WilayahController.php
@@ -5,6 +5,7 @@ namespace Modules\Admin\Wilayah\Controllers;
 use Modules\Admin\Wilayah\Models\WilayahModel;
 use Modules\Shared\Core\Controllers\BaseWebController;
 use PhpOffice\PhpSpreadsheet\Spreadsheet;
+use PhpOffice\PhpSpreadsheet\Writer\Xlsx;
 
 class WilayahController extends BaseWebController
 {
@@ -53,7 +54,38 @@ class WilayahController extends BaseWebController
     {
         $data = $this->wilayahModel->getList();
         $spreadsheet = new Spreadsheet();
-        print_r($spreadsheet);die();
-    }
+        $sheet       = $spreadsheet->getActiveSheet();
+        foreach(range('A', 'F') as $columnID) {
+            $sheet->getColumnDimension($columnID)
+                ->setAutoSize(true);
+        }
+        $sheet->getStyle('A1:F1')->getFont()->setBold(true);
+        $sheet->setCellValue('A1', '#')
+            ->setCellValue('B1', 'Lokasi')
+            ->setCellValue('C1', 'Longitude')
+            ->setCellValue('D1', 'Latitude')
+            ->setCellValue('E1', 'Jumlah Pekerja')
+            ->setCellValue('F1', 'Created At');
 
+        $startColumn = 2;
+        foreach ($data as $index => $item) {
+            $sheet->setCellValue('A' . $startColumn, $index + 1)
+                ->setCellValue('B' . $startColumn, $item->nama)
+                ->setCellValue('C' . $startColumn, $item->lon)
+                ->setCellValue('D' . $startColumn, $item->lat)
+                ->setCellValue('E' . $startColumn, $item->total_pekerja)
+                ->setCellValue('F' . $startColumn, $item->created_at);
+            $startColumn++;
+        }
+
+        $writer = new Xlsx($spreadsheet);
+        $filename = time() . '_data_perwilayah';
+
+        header('Content-Type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
+        header('Content-Disposition: attachment;filename=' . $filename . '.xlsx');
+        header('Cache-Control: max-age=0');
+
+        $writer->save('php://output');
+        die();
+    }
 }
--- sitanink-webfiles/modules/admin/wilayah/models/WilayahModel.php
+++ sitanink-webfiles/modules/admin/wilayah/models/WilayahModel.php
@@ -19,14 +19,15 @@ class WilayahModel
         $data = $this->db
             ->table('lokasi_kerja')
             ->select('
-            lokasi_kerja.id,
-            lokasi_kerja.nama,
-            lokasi_kerja.lon,
-            lokasi_kerja.lat,
-            lokasi_kerja.created_at,
-            count(pekerja.id) as total_pekerja,
-        ')
+                lokasi_kerja.id,
+                lokasi_kerja.nama,
+                lokasi_kerja.lon,
+                lokasi_kerja.lat,
+                lokasi_kerja.created_at,
+                count(pekerja.id) as total_pekerja,
+            ')
             ->join('pekerja', 'lokasi_kerja.id = pekerja.id_lokasi_kerja', 'left')
+            ->groupBy('pekerja.id_lokasi_kerja')
             ->get()
             ->getResultObject();
 
